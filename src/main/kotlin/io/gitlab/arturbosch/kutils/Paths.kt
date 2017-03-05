dcf2313ae1e8ce3de7e63c0ad43c589f6e72261b
abosch <arturbosch@gmx.de>
2017-03-05 21:30:53 +0100

    Add more Path extensions

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Paths.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Paths.kt
@@ -17,3 +17,28 @@ fun Path.isDirectory(): Boolean = Files.isDirectory(this)
  * Tests if this path has the given ending.
  */
 fun Path.hasEnding(ending: String): Boolean = this.toAbsolutePath().toString().endsWith(ending)
+
+/**
+ * Tests if this path exists.
+ */
+fun Path.exists() = Files.exists(this)
+
+/**
+ * Tests if this path does not exist.
+ */
+fun Path.notExists() = Files.notExists(this)
+
+/**
+ * Reads the file content into a string.
+ */
+fun Path.readText() = String(Files.readAllBytes(this))
+
+/**
+ * Reads all lines of the file represented by this path.
+ */
+fun Path.readLines() = Files.readAllLines(this)
+
+/**
+ * Copies content of this path to target path.
+ */
+fun Path.copy(target: Path) = Files.copy(this, target)
