0ccfac174d7dc7ce9934734870925522c6968729
abosch <arturbosch@gmx.de>
2017-04-03 10:24:59 +0200

    Counts the frequency of a part in a string

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Strings.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Strings.kt
@@ -7,3 +7,23 @@ fun String.replaceLast(oldChars: String, newChars: String, ignoreCase: Boolean =
 	val index = lastIndexOf(oldChars, ignoreCase = ignoreCase)
 	return if (index < 0) this else this.replaceRange(index, index + oldChars.length, newChars)
 }
+
+/**
+ * Counts how often a specific part is inside the source string.
+ */
+fun frequency(source: String, part: String): Int {
+
+	if (source.isEmpty() || part.isEmpty()) {
+		return 0
+	}
+
+	var count = 0
+	var pos = source.indexOf(part, 0)
+	while (pos != -1) {
+		pos += part.length
+		count++
+		pos = source.indexOf(part, pos)
+	}
+
+	return count
+}
