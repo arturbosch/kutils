d1488bb794716bf132ceeb36d8a39f764cc2c412
Artur Bosch <arturbosch@gmx.de>
2018-09-08 14:57:03 +0200

    Delete xor as kotlin already has it

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Truth.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Truth.kt
@@ -9,6 +9,3 @@ fun Any?.notNull(): Boolean = this != null
  * Shorthand for null check for booleans.
  */
 fun Boolean?.isTrue(): Boolean = this != null && this
-
-@Suppress("NOTHING_TO_INLINE")
-inline infix fun Boolean.xor(other: Boolean): Boolean = this && !other || !this && other
