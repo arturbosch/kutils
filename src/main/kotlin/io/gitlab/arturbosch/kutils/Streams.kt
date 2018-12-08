f23d13c21071cdc0ddb83046cdbb84979c670a31
Artur Bosch <arturbosch@gmx.de>
2018-12-08 15:24:56 +0100

    Add more java stream interop function

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Streams.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Streams.kt
@@ -1,9 +1,26 @@
 package io.gitlab.arturbosch.kutils
 
+import java.util.Arrays
 import java.util.stream.Collectors
 import java.util.stream.Stream
 
 /**
- * Consumes the stream and collects entries to a list.
+ * Collect entries of stream into a list.
  */
 fun <T> Stream<T>.toList(): List<T> = collect(Collectors.toList<T>())
+
+/**
+ * Collect entries of stream into a set.
+ */
+fun <T> Stream<T>.toSet(): Set<T> = collect(Collectors.toSet<T>())
+
+/**
+ * Collect entries of stream into a collection providing by the factory.
+ */
+fun <T, C : MutableCollection<T>> Stream<T>.into(factory: () -> C): C =
+        collect(Collectors.toCollection { factory.invoke() })
+
+/**
+ * Converts this array to a java stream.
+ */
+fun <T> Array<T>.stream(): Stream<T> = Arrays.stream(this)
---
+++ src/test/kotlin/io/gitlab/arturbosch/kutils/StreamsKtSpec.kt
@@ -0,0 +1,28 @@
+package io.gitlab.arturbosch.kutils
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+/**
+ * @author Artur Bosch
+ */
+internal class StreamsKtSpec : StringSpec({
+
+    "stream into list" {
+        arrayOf(1, 2, 3)
+                .stream()
+                .toList() shouldBe listOf(1, 2, 3)
+    }
+
+    "stream into set" {
+        arrayOf(1, 2, 3)
+                .stream()
+                .toSet() shouldBe setOf(1, 2, 3)
+    }
+
+    "stream into custom collection" {
+        arrayOf(1, 2, 3)
+                .stream()
+                .into { LinkedHashSet() } shouldBe linkedSetOf(1, 2, 3)
+    }
+})
