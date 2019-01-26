3823589837f45459bef92c9e30ad6048e3fd047e
Artur Bosch <arturbosch@gmx.de>
2019-01-26 12:36:55 +0100

    Fix max line length issues

--- src/main/kotlin/io/gitlab/arturbosch/kutils/args/Exceptions.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/args/Exceptions.kt
@@ -2,16 +2,34 @@ package io.gitlab.arturbosch.kutils.args
 
 abstract class OptionException(description: String) : Exception(description)
 
-class NotFlagException(options: String, option: Option<*>) : UnknownOptionException(options, "Illegal option: -" + option.shortForm + " requires a value in '" + options + "'")
+class NotFlagException(
+    options: String,
+    option: Option<*>
+) : UnknownOptionException(
+        options,
+        "Illegal option: -" + option.shortForm + " requires a value in '" + options + "'"
+)
 
-class IllegalOptionValueException(option: Option<*>, val value: String) : OptionException("Illegal value '" + value + "' for option " + option.toString())
+class IllegalOptionValueException(
+    option: Option<*>,
+    val value: String
+) : OptionException("Illegal value '$value' for option $option")
 
-class IllegalOptionNameException(option: Option<*>) : OptionException("Illegal name option " + option.toString())
+class IllegalOptionNameException(option: Option<*>) : OptionException("Illegal name option $option")
 
-class RequiredOptionException(option: Option<*>) : OptionException("Option " + option.toString() + " is required ")
+class RequiredOptionException(option: Option<*>) : OptionException("Option $option is required ")
 
-class UnsetOptionException(option: Option<*>) : OptionException("Option " + option.toString() + " is not yet parsed.")
+class UnsetOptionException(option: Option<*>) : OptionException("Option $option is not yet parsed.")
 
-open class UnknownOptionException(optionLong: String, description: String = "Unknown option '$optionLong'") : OptionException(description)
+open class UnknownOptionException(
+    optionLong: String,
+    description: String = "Unknown option '$optionLong'"
+) : OptionException(description)
 
-class UnknownSubOptionException(options: String, subOption: Char) : UnknownOptionException(options, "Unknown option: '$subOption' in '$options'")
+class UnknownSubOptionException(
+    options: String,
+    subOption: Char
+) : UnknownOptionException(
+        options,
+        "Unknown option: '$subOption' in '$options'"
+)
