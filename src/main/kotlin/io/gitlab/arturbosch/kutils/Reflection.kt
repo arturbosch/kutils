0e526f681dcd523468000da017fff59d21eb5675
Artur Bosch <arturbosch@gmx.de>
2019-01-26 11:48:14 +0100

    Add shortcut function for retrieving the simple name of a class

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Reflection.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Reflection.kt
@@ -4,3 +4,8 @@ package io.gitlab.arturbosch.kutils
  * Not so ugly way to get a qualified class name.
  */
 inline fun <reified T> className(): String = T::class.java.name
+
+/**
+ * Not so ugly way to get a simple class name.
+ */
+inline fun <reified T> simpleClassName(): String = T::class.java.simpleName
