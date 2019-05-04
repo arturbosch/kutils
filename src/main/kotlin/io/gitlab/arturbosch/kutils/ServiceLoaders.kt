86260940bbed337009a56eb733131793eff6cd85
Artur Bosch <arturbosch@gmx.de>
2019-05-04 11:31:49 +0200

    Add classLoader as parameter for loading services

--- src/main/kotlin/io/gitlab/arturbosch/kutils/ServiceLoaders.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/ServiceLoaders.kt
@@ -2,4 +2,6 @@ package io.gitlab.arturbosch.kutils
 
 import java.util.ServiceLoader
 
-inline fun <reified T> load(): ServiceLoader<T> = ServiceLoader.load(T::class.java)
+inline fun <reified T> load(classLoader: ClassLoader? = null): ServiceLoader<T> =
+    ServiceLoader.load(T::class.java, classLoader ?: T::class.java.classLoader)
+
