9a3e985c36af0f9b19d419dfa4ebd66498138e7d
Artur Bosch <arturbosch@gmx.de>
2019-05-06 23:12:00 +0200

    Add positive functions for long and floats

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Numbers.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Numbers.kt
@@ -2,4 +2,8 @@ package io.gitlab.arturbosch.kutils
 
 fun Int.positive() = if (this < 0) 0 else this
 
+fun Long.positive() = if (this < 0L) 0L else this
+
+fun Float.positive() = if (this < 0.0f) 0.0f else this
+
 fun Double.positive() = if (this < 0.0) 0.0 else this
--- src/test/kotlin/io/gitlab/arturbosch/kutils/NumbersSpec.kt
+++ src/test/kotlin/io/gitlab/arturbosch/kutils/NumbersSpec.kt
@@ -8,10 +8,22 @@ import io.kotlintest.specs.StringSpec
  */
 internal class NumbersSpec : StringSpec({
 
-    "positive" {
+    "positive ints" {
         (-1).positive() shouldBe 0
         0.positive() shouldBe 0
         100.positive() shouldBe 100
+    }
+
+    "positive longs" {
+        (-1L).positive() shouldBe 0L
+        0L.positive() shouldBe 0L
+        100L.positive() shouldBe 100L
+    }
+
+    "positive floats" {
+        (-1.0f).positive() shouldBe 0.0f
+        0.0f.positive() shouldBe 0.0f
+        100.0f.positive() shouldBe 100.0f
     }
 
     "positive doubles" {
