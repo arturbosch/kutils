4f6e0b4465b1ee7f58eb08e6e92244eb71779e84
Artur Bosch <arturbosch@gmx.de>
2019-06-10 22:37:57 +0200

    Add utility methods to convert dates to instants

--- src/main/kotlin/io/gitlab/arturbosch/kutils/Dates.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/Dates.kt
@@ -1,14 +1,19 @@
 package io.gitlab.arturbosch.kutils
 
+import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.Date
 
-fun LocalDate.toDate(): Date = Date.from(this.atStartOfDay(ZoneId.systemDefault()).toInstant())
+fun LocalDate.toDate(): Date = Date.from(toInstant())
 
-fun LocalDateTime.toDate(): Date = Date.from(this.atZone(ZoneId.systemDefault()).toInstant())
+fun LocalDate.toInstant(): Instant = this.atStartOfDay(ZoneId.systemDefault()).toInstant()
+
+fun LocalDateTime.toDate(): Date = Date.from(toInstant())
+
+fun LocalDateTime.toInstant(): Instant = this.atZone(ZoneId.systemDefault()).toInstant()
 
 fun ZonedDateTime.toDate(): Date = Date.from(this.toInstant())
 
@@ -17,6 +22,12 @@ fun Date.toLocalDate(): LocalDate = this.toInstant().atZone(ZoneId.systemDefault
 fun Date.toLocalDateTime(): LocalDateTime = this.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
 
 fun Date.toZonedDateTime(): ZonedDateTime = this.toInstant().atZone(ZoneId.systemDefault())
+
+fun Instant.toLocalDate(): LocalDate = this.atZone(ZoneId.systemDefault()).toLocalDate()
+
+fun Instant.toLocalDateTime(): LocalDateTime = this.atZone(ZoneId.systemDefault()).toLocalDateTime()
+
+fun Instant.toZonedDateTime(): ZonedDateTime = this.atZone(ZoneId.systemDefault())
 
 fun LocalDate.startOfWeek(): LocalDate = minusDays(dayOfWeek.value - 1L)
 
