253dd2c3d6b022cc7cc03d83521f8c15aa0de66e
Artur Bosch <arturbosch@gmx.de>
2019-07-28 21:42:15 +0200

    Simplify last element chain

--- src/main/kotlin/io/gitlab/arturbosch/kutils/ServiceLoaders.kt
+++ src/main/kotlin/io/gitlab/arturbosch/kutils/ServiceLoaders.kt
@@ -5,4 +5,4 @@ import java.util.ServiceLoader
 inline fun <reified T> load(classLoader: ClassLoader? = null): ServiceLoader<T> =
     ServiceLoader.load(T::class.java, classLoader ?: T::class.java.classLoader)
 
-fun <T : WithPriority> ServiceLoader<T>.firstPrioritized(): T? = sortedBy { it.priority }.lastOrNull()
+fun <T : WithPriority> ServiceLoader<T>.firstPrioritized(): T? = maxBy { it.priority }
