cc6ec5b06487fc49ac660204d0cfe29e17d12cc7
Artur Bosch <arturbosch@gmx.de>
2019-09-22 18:22:04 +0200

    Repair flaky testcase by computing the string distance instead of plain equals

--- src/test/kotlin/io/gitlab/arturbosch/kutils/ConsoleTreesSpec.kt
+++ src/test/kotlin/io/gitlab/arturbosch/kutils/ConsoleTreesSpec.kt
@@ -5,33 +5,44 @@ import io.kotlintest.specs.StringSpec
 
 class ConsoleTreesSpec : StringSpec({
 
+    class Node(
+        override val label: String,
+        override val children: List<ConsoleTreeNode>
+    ) : ConsoleTreeNode
+
+    fun assertLinesAreEqual(actual: String, expected: String) {
+        actual.trimIndent().split(System.lineSeparator())
+            .zip(expected.trimIndent().split(System.lineSeparator()))
+            .map { it.first.trim() to it.second.trim() }
+            .forEach { it.first shouldBe it.second }
+    }
+
     "simple testcase" {
-        val root = Node(
-            "A",
-            listOf(
-                Node("B", listOf(Node("C", listOf()))),
-                Node("D", listOf())
-            )
-        )
-
-        val content = toTreeString(root)
-
-        content.trimIndent() shouldBe """
+        assertLinesAreEqual(
+            toTreeString(Node(
+                "A",
+                listOf(
+                    Node("B", listOf(Node("C", listOf()))),
+                    Node("D", listOf())
+                )
+            )),
+            """
              ── A
                 ├── B
                 │   └── C
                 └── D
-        """.trimIndent()
+            """
+        )
     }
 
-    "path based testcase" {
-        val content = toTreeString(
-            resourceAsPath("consoleTree"),
-            DefaultConsoleTreeConverters.PathConverter()
-        )
-
-        content.trimIndent() shouldBe """
-            ── consoleTree
+    "path based tree" {
+        assertLinesAreEqual(
+            toTreeString(
+                resourceAsPath("consoleTree"),
+                DefaultConsoleTreeConverters.PathConverter()
+            ),
+            """
+             ── consoleTree
                ├── A
                │   ├── file1
                │   ├── file4
@@ -39,12 +50,7 @@ class ConsoleTreesSpec : StringSpec({
                │       └── file2
                └── B
                    └── file3
-        """.trimIndent()
+            """
+        )
     }
 })
-
-class Node(
-    override val label: String,
-    override val children: List<ConsoleTreeNode>
-) : ConsoleTreeNode
-
